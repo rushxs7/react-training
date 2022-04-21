3d228f73cbb3ae496638587a35a4f83b41f4b96b
Rushil Ramautar <rushil.ramautar@datasur.sr>
2022-04-21 13:16:20 -0300

    added lesson-5: store components in seperate files

---
+++ lesson-5/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+                &copy; 2022. Rushil Ramautar Development. All rights reserved.
+            </div>
+        </footer>
+    )
+}
---
+++ lesson-5/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className='nav'>
+            <img className='nav-logo' src="/react-logo.png" width="80px" />
+            <ul>
+                <li>The</li>
+                <li>Fucking</li>
+                <li>Job</li>
+                <li>Requires</li>
+                <li>It</li>
+            </ul>
+        </nav>
+    )
+}
